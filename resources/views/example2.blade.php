d47c9a75e554d7e6aa79335118023ce5f7dbc8e3
Alexandre Iavriani <aleksandre.iavrian@gau.edu.ge>
2021-12-07 00:07:21 +0400

    my changes

---
+++ resources/views/example2.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+<h2>Hello {{ $name }}</h2>
+<h2>Age {{ $Age }}</h2>
+    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,32 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/test1', function () {
+//     return view('example');
+// });
+
+Route::get('/test1', function () {
+    return view('example', ['name' => "Alex"]);
+});
+
+Route::get('/test1_2/{name}', function ($name) {
+    return view('example', ['name' => $name]);
+});
+
+if (View::exists('example')) {
+    //
+
+}
+
+Route::get('/test2', function () {
+    return view('example2')
+            ->with('name', 'Victoria')
+            ->with('Age', 24);
+});
+
+Route::get('/test3', function ($name) {
+    return view('example2')
+            ->with('name', 'Victoria')
+            ->with('Age', 24);
+});
